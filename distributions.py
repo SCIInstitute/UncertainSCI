6e69c8131ba12f43c0c0c102930792d64c7b0373
Akil Narayan <akil.narayan@gmail.com>
2020-04-22 11:52:11 -0600

    Cleanup for beta distribution constructor

--- distributions.py
+++ distributions.py
@@ -10,48 +10,26 @@ class ProbabilityDistribution:
         pass
 
 class BetaDistribution(ProbabilityDistribution):
-    def __init__(self, alpha=None, beta=None, mean=None, stdev=None, dim=1, domain=None):
+    def __init__(self, alpha=None, beta=None, mean=None, stdev=None, dim=None, domain=None):
 
         # Convert mean/stdev inputs to alpha/beta
         if mean is not None and stdev is not None:
             if alpha is not None or beta is not None:
                 raise ValueError('Cannot simultaneously specify alpha/beta parameters and mean/stdev parameters')
 
-            alpha, beta = self._convert_meanstdev_to_alphabeta(alpha, beta)
+            alpha, beta = self._convert_meanstdev_to_alphabeta(mean, stdev)
 
-        # Tons of type/value checking for alpha/beta vs dim
-        alphiter = isinstance(alpha, (list, tuple))
-        betaiter = isinstance(beta, (list, tuple))
-        if alphiter and betaiter:
-            if len(alpha) > 1 and len(beta) > 1:
-                assert len(alpha) == len(beta), "Alpha and Beta parameter inputs must be of the same size"
-            elif len(alpha) == 1 and len(beta) == 1:
-                pass
-            elif len(alpha) == 1:
-                alpha = [alpha[0] for i in range(len(beta))]
-            elif len(beta) == 1:
-                beta = [beta[0] for i in range(len(alpha))]
-        elif alphiter:
-            beta = [beta for i in range(len(alpha))]
-        elif betaiter:
-            alpha = [alpha for i in range(len(beta))]
-        else:
-            alpha, beta = [alpha,], [beta,]
+        alpha, beta = self._convert_alphabeta_to_iterable(alpha, beta)
 
-        if dim > 1:
-            if len(alpha) == 1:
-                alpha = [alpha[0] for i in range(dim)]
-                beta = [beta[0] for i in range(dim)]
-            elif len(alpha) != dim or len(beta) != dim:
-                raise ValueError("User-specified dimension must be consistent with input parameters (alpha,beta)")
-        else:
-            dim = len(alpha)
+        # Sets self.dim, self.alpha, self.beta, self.domain
+        self._detect_dimension(alpha, beta, dim, domain)
 
-        for qd in range(dim):
-            assert alpha[qd]>0 and beta[qd]>0, "Parameter vectors alpha and beta must have strictly positive components"
-        assert dim > 0
+        for qd in range(self.dim):
+            assert self.alpha[qd]>0 and self.beta[qd]>0, "Parameter vectors alpha and beta must have strictly positive components"
+        assert self.dim > 0, "Dimension must be positive"
+        assert self.domain.shape == (2, self.dim)
 
-        self.alpha, self.beta, self.dim = alpha, beta, dim
+        ## Construct affine map transformations
 
         # Standard domain is [0,1]^dim
         self.standard_domain = np.ones([2, self.dim])
@@ -63,27 +41,93 @@ class BetaDistribution(ProbabilityDistribution):
         self.jacobi_domain[0,:] = -1.
         self.transform_standard_dist_to_poly = AffineTransform(domain=self.standard_domain, image=self.jacobi_domain)
 
-        # "Physical" domain is whatever user inputs
-        if domain is None:
-            self.domain = self.standard_domain.copy()
-            self.domain[0,:] = 0.
-        else:
-            if self.dim == 1:
-                assert len(domain) == 2
-                domain = np.array(domain).reshape(2, 1)
-            else:
-                assert domain.shape == (2, self.dim)
-
         self.transform_to_standard = AffineTransform(domain=self.domain, image=self.standard_domain)
 
+        ## Construct 1D polynomial families
         Js = []
         for qd in range(self.dim):
-            Js.append(JacobiPolynomials(alpha=beta[qd]-1., beta=alpha[qd]-1.))
+            Js.append(JacobiPolynomials(alpha=self.beta[qd]-1., beta=self.alpha[qd]-1.))
         self.polys = TensorialPolynomials(polys1d=Js)
 
         self.indices = None
 
-    def _convert_meanstdev_to_alphabeta(self, mean, stdev, dim):
+    def _detect_dimension(self, alpha, beta, dim, domain):
+        """
+        Parses user-given inputs to determine the dimension of the distribution.
+
+        alpha and beta are iterables.
+
+        Sets self.dim, self.alpha, self.beta, and self.domain
+        """
+
+        # Situations: 
+        # 1. User specifies alpha, beta as lists (disallow contradictory
+        #    dimension, domain specification)
+        # 2. User specifies dim scalar (disallow contradictory alpha, beta,
+        #    domain specification) 
+        # 3. User specifies domain hypercube
+
+        if len(alpha) > 1 or len(beta) > 1: # Case 1:
+            if len(alpha) != len(beta):
+                raise ValueError('Input parameters alpha and beta must be the same dimension')
+
+            if (dim is not None) and (dim != 1) and (dim != len(alpha)):
+                raise ValueError('Alpha, beta parameter lists must have size consistent with input dim')
+
+            if (domain is not None) and (domain.shape[1] !=  1) and (domain.shape[1] != len(alpha)):
+                raise ValueError('Alpha, beta parameter lists must have size consistent with hypercube domain')
+
+            self.dim = len(alpha)
+            self.alpha = alpha
+            self.beta = beta
+
+            if domain is None: # Standard domain [0,1]^dim
+                self.domain = np.zeros([2, self.dim])
+                self.domain[1,:] = 1.
+            else:
+                if domain.shape[1] == 1: # Tensorize 1D domain
+                    self.domain = np.zeros([2, self.dim])
+                    self.domain[0,:] = domain[0]
+                    self.domain[1,:] = domain[1]
+                else:  # User specified domain
+                    self.domain = domain
+
+        elif dim is not None and dim > 1: # Case 2
+            self.dim = dim
+            self.alpha = [alpha[0] for i in range(self.dim)]
+            self.beta = [beta[0] for i in range(self.dim)]
+
+            if domain is None: # Standard domain [0,1]^dim
+                self.domain = np.zeros([2, self.dim])
+                self.domain[1,:] = 1.
+            else:
+                if domain.shape[1] == 1: # Tensorize 1D domain
+                    self.domain = np.zeros([2, self.dim])
+                    self.domain[0,:] = domain[0]
+                    self.domain[1,:] = domain[1]
+                else:  # User specified domain
+                    self.domain = domain
+
+            return 
+
+        elif domain is not None and domain.shape[1] > 1: # Case 3
+            self.dim = domain.shape[1]
+            self.alpha = [alpha[0] for i in range(self.dim)]
+            self.beta = [beta[0] for i in range(self.dim)]
+            self.domain = domain
+
+        else: # The dimension is 1
+            self.dim = 1
+            self.alpha = alpha
+            self.beta = beta
+
+            if domain is None:
+                self.domain = np.zeros([2, self.dim])
+                self.domain[1,:] = 1.
+            else:
+                self.domain = domain.reshape([2, self.dim])
+
+    def _convert_meanstdev_to_alphabeta(self, mean, stdev):
         """
         Converts user-given mean and stdev to an iterable alpha, beta.
         """
@@ -124,6 +168,45 @@ class BetaDistribution(ProbabilityDistribution):
         # If they're both scalars, let the following alpha/beta checker cf vs dim
         else:
             alpha, beta = self.meanstdev_to_alphabeta(mean, stdev)
+
+        return alpha, beta
+
+    def _convert_alphabeta_to_iterable(self, alpha, beta):
+        """
+        Converts user-input (alpha,beta) to iterables. Ensures that the length
+        of the iterables matches on output.
+
+        If alpha or beta are None, sets those values to 1.
+        """
+
+        # Tons of type/value checking for alpha/beta vs dim
+        alphiter = isinstance(alpha, (list, tuple))
+        betaiter = isinstance(beta, (list, tuple))
+        if alphiter and betaiter:
+            if len(alpha) > 1 and len(beta) > 1:
+                assert len(alpha) == len(beta), "Alpha and Beta parameter inputs must be of the same size"
+            elif len(alpha) == 1 and len(beta) == 1:
+                pass
+            elif len(alpha) == 1:
+                alpha = [alpha[0] for i in range(len(beta))]
+            elif len(beta) == 1:
+                beta = [beta[0] for i in range(len(alpha))]
+
+        elif alphiter: # alpha is iterable, beta is not
+            if beta is None:
+                beta = 1.
+            beta = [beta for i in range(len(alpha))]
+
+        elif betaiter: # beta is iterable, alpha is not
+            if alpha is None:
+                alpha = 1.
+            alpha = [alpha for i in range(len(beta))]
+
+        elif alpha is None and beta is None:
+            alpha, beta = [1.,], [1.,]
+
+        else:  # alpha, beta should both be scalars
+            alpha, beta = [alpha,], [beta,]
 
         return alpha, beta
 
