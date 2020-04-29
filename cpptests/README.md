89b8f838943b9affabe834d91e79ecfde881a3bf
Daniel White <dwhite@sci.utah.edu>
2020-04-29 12:34:43 -0600

    Template cmake project

---
+++ cpptests/README.md
@@ -0,0 +1,1 @@
+# CMake C++ Project Template
---
+++ cpptests/src/main.cpp
@@ -0,0 +1,28 @@
+#include <division.h>
+#include "main.h"
+
+int main(int argc, const char *argv[]) {
+  Fraction f;
+
+  std::cout << HEADER;
+
+  // ensure the correct number of parameters are used.
+  if (argc < 3) {
+    std::cout << USAGE;
+    return 1;
+  }
+
+  f.numerator = atoll(argv[1]);
+  f.denominator = atoll(argv[2]);
+
+  Division d = Division(f);
+  try {
+    DivisionResult r = d.divide();
+
+    std::cout << "Division : " << f.numerator << " / " << f.denominator << " = " << r.division << "\n";
+    std::cout << "Remainder: " << f.numerator << " % " << f.denominator << " = " << r.remainder << "\n";
+  } catch (DivisionByZero) {
+    std::cout << "Can not divide by zero, Homer. Sober up!\n";
+  }
+  return 0;
+}
