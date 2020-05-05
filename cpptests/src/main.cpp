21e98b0760b905a4e222df23d035650bab23af5a
Daniel White <dwhite@sci.utah.edu>
2020-05-05 10:25:40 -0600

    Change to use simpler test code

--- cpptests/src/main.cpp
+++ cpptests/src/main.cpp
@@ -12,12 +12,15 @@ int main(int argc, const char *argv[])
     return 1;
   }
 
-  JacobiPolynomials J;
+  auto alpha = 0.0;
+  auto beta = 0.0;
   auto N = 100;
   auto k = 15;
 
-  auto [x,w] = J.gauss_quadrature(N);
-  auto V = J.eval(x, range(k));
+  auto ab = jacobi_recurrence_values(N, alpha, beta);
+
+  auto [x,w] = gauss_quadrature_driver(ab, N);
+  auto V = eval_driver(x, np.arange(k), 0, ab);
 
   //plt.plot(x, V[:,:k])
   //plt.show()
