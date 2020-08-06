2ff0da6bcfac03d740a193c336f1040216601b93
Akil Narayan <akil.narayan@gmail.com>
2020-08-06 09:37:03 -0600

    1D ODE test

--- model_examples.py
+++ model_examples.py
@@ -4,6 +4,8 @@
 # of the model output.
 
 import numpy as np
+from scipy import sparse
+from scipy.sparse import linalg as splinalg
 
 def taylor_frequency(p):
     """
@@ -27,49 +29,122 @@ def sine_modulation(left=-1, right=1, N=100):
 
     return lambda p: np.sin(np.pi * x * taylor_frequency(p))
 
+def laplace_ode_diffusion(x, p):
+    """ Parameterized diffusion coefficient for 1D ODE
+
+    For a d-dimensional parameter p, the diffusion coefficient a(x,p) has the form
+    
+      a(x,p) = pi^2/5 + sum_{j=1}^d p_j * sin(j*pi*(x+1)/2) / j^2,
+
+    which is positive for all x if all values of p lie between [-1,1].
+    """
+
+    a_val = np.ones(x.shape)*np.pi**2/5
+    for q in range(p.size):
+        a_val += p[q] * np.sin((q+1)*np.pi*(x+1)/2)/(q+1)**2
+    return a_val
+
+def laplace_grid_x(left, right, N):
+    """
+    Computes one-dimensional equispaced grid with N points on the interval
+    (left, right).
+    """
+    return np.linspace(left, right, N)
+
 def laplace_ode(left=-1., right=1., N=100, f=None):
     """
 
-    Computes the solution to the periodic ODE:
+    Computes the solution to the ODE:
 
       -d/dx [ a(x,p) d/dx u(x,p) ] = f(x),
 
-    where, for a d-dimensional parameter p, a(x,p) has the form
+    with homogeneous Dirichlet boundary conditions at x = left, x = right.
+
+    For a d-dimensional parameter p, a(x,p) is the function defined in laplace_ode_diffusion.
     
-      a(x,p) = pi^2/5 + sum_{j=1}^d p_j * sin(j*pi*x) / j^2,
-
-    which is positive for all x if all values of p lie between [-1,1].
-
-    Uses a finite-difference discretization of the ODE.
+    Uses an equispaced finite-difference discretization of the ODE.
 
     """
 
-    if f is None:
-        f = lambda x: np.pi**2 * np.sin(np.pi*x)
+    assert N > 2
 
-    x = np.linspace(left, right, N+1)
+    if f is None:
+        f = lambda x: np.pi**2 * np.cos(np.pi*x)
+
+    x = laplace_grid_x(left, right, N)
     h = x[1] - x[0]
-    x = x[:-1] + 0.5*h
     fx = f(x)
 
-    def a_eval(x, p):
-        a_val = np.ones(x.shape)*np.pi**2/5
-        for q in range(p.size):
-            a_val += p[q] * np.sin((q+1)*np.pi*x)/(q+1)**2
-        return a_val
+    # Set homogeneous Dirichlet conditions
+    fx[0], fx[-1] = 0., 0.
+
+    # i+1/2 points
+    xh = x[:-1] + h/2.
 
     def create_system(p):
-        nonlocal x
-        a = a_eval(x, p)
-        A = np.diag(a[1:], k=1) + np.diag(a[1:], k=-1) - (np.diag(np.roll(a, 1) + a))
-        A[-1,0] = a[0]
-        A[0,-1] = a[0]
+        nonlocal x, xh, N
+        a = laplace_ode_diffusion(xh, p)
+        number_nonzeros = 1 + 1 + (N-2)*3
+        rows = np.zeros(number_nonzeros, dtype=int)
+        cols = np.zeros(number_nonzeros, dtype=int)
+        vals = np.zeros(number_nonzeros, dtype=float)
+
+        # Set the homogeneous Dirichlet conditions
+        rows[0], cols[0], vals[0] = 0, 0, 1.
+        rows[1], cols[1], vals[1] = N-1, N-1, 1.
+        ind = 2
+
+        for q in range(1, N-1):
+            # Column q-1
+            rows[ind], cols[ind], vals[ind] = q, q-1, -a[q-1]
+            ind += 1
+
+            # Column q
+            rows[ind], cols[ind], vals[ind] = q, q, a[q-1] + a[q]
+            ind += 1
+
+            # Column q+1
+            rows[ind], cols[ind], vals[ind] = q, q+1, -a[q]
+            ind += 1
+
+        A = sparse.csc_matrix((vals, (rows, cols)), shape=(N,N))
+
         return A
 
     def solve_system(p):
-        return np.linalg.solve(create_system(p), fx*(h**2))
+        nonlocal fx, h
+        return splinalg.spsolve(create_system(p), fx*(h**2))
 
     return lambda p: solve_system(p)
+
+def laplace_grid_xy(left, right, N1, down, up, N2):
+    """
+    Computes two-dimensional tensorial equispaced grid corresponding to the
+    tensorization of N1 equispaced points on the interval (left, right) and N2
+    equispaced points on the interval (down, up).
+    """
+    x = np.linspace(left, right, N1)
+    y = np.linspace(down, up, N2)
+
+    X, Y = np.meshgrid(x, y)
+
+    return X.flatten(order='C'), Y.flatten(order='C')
+
+def laplace_pde_diffusion(x, p):
+    """ Parameterized diffusion coefficient for 2D PDE
+
+    For a d-dimensional parameter p, the diffusion coefficient a(x,p) has the form
+    
+      a(x,p) = pi^2/5 + sum_{j=1}^d p_j * sin(j*pi*(x+1)/2) / j^2,
+
+    which is positive for all x if all values of p lie between [-1,1].
+    """
+
+    a_val = np.ones(x.shape)*np.pi**2/5
+    for q in range(p.size):
+        a_val += p[q] * np.sin((q+1)*np.pi*(x+1)/2)/(q+1)**2
+    return a_val
+
 
 def genz_oscillatory(w=0., c=None):
     """
@@ -92,8 +167,25 @@ def genz_oscillatory(w=0., c=None):
 if __name__ == "__main__":
 
     import pdb
-    model = genz_oscillatory(c=4)
+    from matplotlib import pyplot as plt
 
-    p = np.random.random(2)
+    dim = 5
 
+    left = -1.
+    right = 1.
+    N = 100000
+    model = laplace_ode(left=left, right=right, N=N)
+
+    p = np.random.rand(5)*2 - 1
     u = model(p)
+    x = laplace_grid_x(left, right, N)
+    a = laplace_ode_diffusion(x, p)
+
+    plt.subplot(121)
+    plt.plot(x,a)
+    plt.title('Diffusion coefficient')
+
+    plt.subplot(122)
+    plt.plot(x,u)
+    plt.title('Solution u')
+
