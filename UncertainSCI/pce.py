060fae739f24b0219e2347d776b02d714bb7a7cb
Daniel White <dwhite@sci.utah.edu>
2020-09-18 12:34:53 -0600

    Update pce.py

--- UncertainSCI/pce.py
+++ UncertainSCI/pce.py
@@ -7,6 +7,7 @@ from UncertainSCI.distributions import ProbabilityDistribution
 from UncertainSCI.utils.casting import to_numpy_array
 from UncertainSCI.utils.version import version_lessthan
 
+
 class PolynomialChaosExpansion():
     """Base polynomial chaos expansion class.
 
@@ -74,13 +75,13 @@ class PolynomialChaosExpansion():
             p_standard = self.distribution.polys.wafp_sampling(self.indices.indices(), **sampler_options)
 
             # Maps to domain
-            self.samples = self.distribution.transform_to_standard.mapinv( \
-                               self.distribution.transform_standard_dist_to_poly.mapinv(p_standard) )
+            self.samples = self.distribution.transform_to_standard.mapinv(
+                               self.distribution.transform_standard_dist_to_poly.mapinv(p_standard))
         else:
             raise ValueError("Unsupported sample type '{0}' for input sample_type".format(sample_type))
 
     def build_pce_wafp(self, model=None, model_output=None, samples=None, **sampler_options):
-        """Computes PCE coefficients. 
+        """Computes PCE coefficients.
 
         Uses a weighted approximate Fekete point design to compute a
         least-squares collocation solution.
@@ -112,7 +113,7 @@ class PolynomialChaosExpansion():
             if self.samples is None:
                 self.generate_samples('wafp', **sampler_options)
             else:
-                pass # User didn't specify samples now, but did previously
+                pass  # User didn't specify samples now, but did previously
 
         else:
             if samples.shape[1] != self.indices.indices().shape[1]:
@@ -120,18 +121,19 @@ class PolynomialChaosExpansion():
 
             self.samples = samples
 
-        p_standard = self.distribution.transform_standard_dist_to_poly.map( \
-                    self.distribution.transform_to_standard.map( self.samples ) )
+        p_standard = self.distribution.transform_standard_dist_to_poly.map(
+                    self.distribution.transform_to_standard.map(self.samples))
 
         if model_output is None:
 
             for ind in range(self.samples.shape[0]):
                 if model_output is None:
-                    model_output = model(self.samples[ind,:])
+                    model_output = model(self.samples[ind, :])
                     M = model_output.size
-                    model_output = np.concatenate( [ model_output.reshape([1, M]), np.zeros([self.samples.shape[0]-1, M]) ], axis=0 )
+                    model_output = np.concatenate([model_output.reshape([1, M]),
+                                                  np.zeros([self.samples.shape[0]-1, M])], axis=0)
                 else:
-                    model_output[ind,:] = model(self.samples[ind,:])
+                    model_output[ind, :] = model(self.samples[ind, :])
 
         V = self.distribution.polys.eval(p_standard, self.indices.indices())
 
@@ -141,12 +143,12 @@ class PolynomialChaosExpansion():
         model_output = np.multiply(model_output.T, norms).T
 
         if version_lessthan(np, '1.14.0'):
-            coeffs,residuals = np.linalg.lstsq(V, model_output, rcond=-1)[:2]
+            coeffs, residuals = np.linalg.lstsq(V, model_output, rcond=-1)[:2]
         else:
-            coeffs,residuals = np.linalg.lstsq(V, model_output, rcond=None)[:2]
+            coeffs, residuals = np.linalg.lstsq(V, model_output, rcond=None)[:2]
 
         self.coefficients = coeffs
-        self.p = samples # Should get rid of this.
+        self.p = samples  # Should get rid of this.
         self.model_output = np.multiply(model_output.T, 1/norms).T
 
         return residuals
@@ -184,7 +186,7 @@ class PolynomialChaosExpansion():
         """
 
         self.assert_pce_built()
-        return self.coefficients[0,:]
+        return self.coefficients[0, :]
 
     def stdev(self):
         """
@@ -196,7 +198,7 @@ class PolynomialChaosExpansion():
         """
 
         self.assert_pce_built()
-        return np.sqrt(np.sum(self.coefficients[1:,:]**2, axis=0))
+        return np.sqrt(np.sum(self.coefficients[1:, :]**2, axis=0))
 
     def pce_eval(self, p, components=None):
         """Evaluates the PCE at particular parameter locations.
@@ -216,13 +218,13 @@ class PolynomialChaosExpansion():
         """
 
         self.assert_pce_built()
-        p_std = self.distribution.transform_standard_dist_to_poly.map( \
-                    self.distribution.transform_to_standard.map(p) )
+        p_std = self.distribution.transform_standard_dist_to_poly.map(
+                    self.distribution.transform_to_standard.map(p))
 
         if components is None:
-            return np.dot( self.distribution.polys.eval( p_std, self.indices.indices() ), self.coefficients)
+            return np.dot(self.distribution.polys.eval(p_std, self.indices.indices()), self.coefficients)
         else:
-            return np.dot( self.distribution.polys.eval( p_std, self.indices.indices() ), self.coefficients[:,components])
+            return np.dot(self.distribution.polys.eval(p_std, self.indices.indices()), self.coefficients[:, components])
 
     def quantile(self, q, M=100):
         """
@@ -254,7 +256,7 @@ class PolynomialChaosExpansion():
 
         return quantiles
 
-    def total_sensitivity(self, dim_indices = None):
+    def total_sensitivity(self, dim_indices=None):
         """
         Computes total sensitivity associated to dimensions dim_indices from
         PCE coefficients. dim_indices should be a list-type containing
@@ -268,45 +270,45 @@ class PolynomialChaosExpansion():
         if dim_indices is None:
             dim_indices = range(self.distribution.dim)
 
-        dim_indices = np.asarray(dim_indices,dtype=int)
+        dim_indices = np.asarray(dim_indices, dtype=int)
 
         indices = self.indices.indices()
-        variance_rows = np.linalg.norm(indices, axis=1) > 0.
+        # variance_rows = np.linalg.norm(indices, axis=1) > 0.
 
-        #variances = np.sum(self.coefficients[variance_rows,:]**2, axis=0)
+        # variances = np.sum(self.coefficients[variance_rows,:]**2, axis=0)
         variance = self.stdev()**2
         total_sensitivities = np.zeros([dim_indices.size, self.coefficients.shape[1]])
 
-        for (qj,j) in enumerate(dim_indices):
-            total_sensitivities[qj,:] = np.sum(self.coefficients[indices[:,j]>0,:]**2, axis=0)/variance
+        for (qj, j) in enumerate(dim_indices):
+            total_sensitivities[qj, :] = np.sum(self.coefficients[indices[:, j] > 0, :]**2, axis=0) / variance
 
         return total_sensitivities
 
     def global_sensitivity(self, dim_lists=None):
         """
         Computes global sensitivity associated to dimensional indices dim_lists
-        from PCE coefficients. 
+        from PCE coefficients.
 
         dim_lists should be a list of index lists. The global sensitivity for each
         index list is returned.
         The output is len(dim_lists) x self.coefficients.shape[1]
         """
 
-        #unique_rows = np.vstack({tuple(row) for row in lambdas})
-        ## Just making sure
-        #assert unique_rows.shape[0] == lambdas.shape[0]
+        # unique_rows = np.vstack({tuple(row) for row in lambdas})
+        # Just making sure
+        # assert unique_rows.shape[0] == lambdas.shape[0]
 
         indices = self.indices.indices()
-        variance_rows = np.linalg.norm(indices, axis=1) > 0.
-        #assert np.sum(np.invert(variance_rows)) == 1
+        # variance_rows = np.linalg.norm(indices, axis=1) > 0.
+        # assert np.sum(np.invert(variance_rows)) == 1
 
         variance = self.stdev()**2
         global_sensitivities = np.zeros([len(dim_lists), self.coefficients.shape[1]])
         dim = self.distribution.dim
-        for (qj,j) in enumerate(dim_lists):
+        for (qj, j) in enumerate(dim_lists):
             jc = np.setdiff1d(range(dim), j)
-            inds = np.logical_and( np.all(indices[:,j] > 0, axis=1), \
-                                   np.all(indices[:,jc]==0, axis=1) )
-            global_sensitivities[qj,:] = np.sum(self.coefficients[inds,:]**2, axis=0)/variance
+            inds = np.logical_and(np.all(indices[:, j] > 0, axis=1),
+                                  np.all(indices[:, jc] == 0, axis=1))
+            global_sensitivities[qj, :] = np.sum(self.coefficients[inds, :]**2, axis=0)/variance
 
         return global_sensitivities
