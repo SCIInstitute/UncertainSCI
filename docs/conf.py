95f582cec4283b7bd56716da7944f2179c8ead12
nids2001 <nids2001@gmail.com>
2021-04-16 15:39:20 -0400

    Change accent color and font

--- docs/conf.py
+++ docs/conf.py
@@ -80,13 +80,15 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+
+
 html_static_path = ['_static']
 
 html_css_files = ['css/main.css']
 
 html_title = project
 
-html_logo = '_static/logo.png'
+html_logo = '_static/UncertainSCI.png'
 
 html_theme_options = {
     'logo_only': True
