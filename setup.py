cae1fe2eb89acb42223bb0d6be7c59340d57e81c
jessdtate <jess@sci.utah.edu>
2022-10-11 16:36:46 -0600

    update python versions

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
