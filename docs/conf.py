29883d07c16fb5de84491c2136fe8d9086cfcccd
jessdtate <jess@sci.utah.edu>
2023-08-30 11:16:22 -0600

    not loading correct theme

--- docs/conf.py
+++ docs/conf.py
@@ -95,8 +95,9 @@ language = 'python'
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-
-import sphinx_rtd_theme
+if not on_rtd:
+  import sphinx_rtd_theme
+  
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
