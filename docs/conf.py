29486fabd3a2b83ce9181f2644218cb93ed375bb
jessdtate <jess@sci.utah.edu>
2023-09-02 12:37:05 -0600

    apparently, rtd theme does not load on rtd anymore

--- docs/conf.py
+++ docs/conf.py
@@ -95,8 +95,8 @@ language = 'python'
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:
-  import sphinx_rtd_theme
+#if not on_rtd:
+import sphinx_rtd_theme
   
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
