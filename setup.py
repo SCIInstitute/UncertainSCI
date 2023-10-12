3bd6e7d0e452b37e2f223287b754ac08cb0fcb0a
jessdtate <jess@sci.utah.edu>
2023-10-11 18:17:54 -0600

    bump version for pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setup(
     name='UncertainSCI',
-    version='1.0.1',
+    version='1.0.1b',
     author='UncertainSCI Developers',
     author_email='uncertainsci@sci.utah.edu',
     packages=find_packages(),
